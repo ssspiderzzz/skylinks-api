b37a0eb86393210e044c853c18ee78384817d962
Haibin Zhu <50757176+ssspiderzzz@users.noreply.github.com>
2021-05-02 19:40:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ pool.connect((error, client) => {
   }
 });
 
-// App.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+App.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Express Configuration
 App.use(cors());
